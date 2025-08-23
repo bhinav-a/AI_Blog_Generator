41b34dc2fe65bbbb76612aad5223ca15e6d1bafd
bhinav-a <anuragabhinav08@gmail.com>
2025-08-23 22:47:02 +0530

    urls.py

--- Bloger/scraper/urls.py
+++ Bloger/scraper/urls.py
@@ -6,6 +6,6 @@ app_name = 'scraper'
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:pk>/', views.detail, name='detail'),
-    path('download/<int:pk>/', views.download_json, name='download'),
+    path('download/<int:pk>/', views.download_json, name='download_json'),
     path('api/scrape/', views.api_scrape, name='api_scrape'),
 ]
