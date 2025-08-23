f2a448e71d2a82782c4a615770809715f09d69fd
bhinav-a <anuragabhinav08@gmail.com>
2025-08-23 22:35:08 +0530

    env

--- Bloger/Bloger/settings.py
+++ Bloger/Bloger/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0bam(m2^)sqiy_6xrbzf3hldkra8=k8i^g=n!n6ksuoi$_-89j')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Security settings for production
 if not DEBUG:
