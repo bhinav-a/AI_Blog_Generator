d72a1ac02f9f7683d5611752ed54bfcf2aee6a23
bhinav-a <anuragabhinav08@gmail.com>
2025-08-23 23:53:33 +0530

    download

--- Bloger/scraper/urls.py
+++ Bloger/scraper/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<str:pk>/', views.detail, name='detail'),
     path('download/<str:pk>/', views.download_json, name='download'),
+    path('download/', views.index, name='download_empty'),
     path('api/scrape/', views.api_scrape, name='api_scrape'),
 ]
